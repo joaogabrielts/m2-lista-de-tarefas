5ef8b597ffbc1eeb42e1ebbaec320f4ea1d010f0
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2024-04-23 13:24:43 -0300

    finalizando javascript

--- script.js
+++ script.js
@@ -10,3 +10,108 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+
+
+
+
+function renderElements(arry){
+  
+  const ul = document.querySelector('ul')
+
+  ul.innerHTML = ''
+
+  for(let i = 0; i < arry.length; i++){
+  
+    const task = arry[i]
+  
+    const taskItem = createTaskItem(arry[i].title, arry[i].type)
+    console.log(taskItem);
+    
+   ul.appendChild(taskItem);
+    
+  }
+  
+}
+renderElements(tasks);
+
+
+
+
+
+function createTaskItem(title,type){
+  const li = document.createElement('li')
+  const div = document.createElement('div')
+  const span = document.createElement('span')
+  const p = document.createElement('p')
+  const button = document.createElement('button')
+
+  
+   li.classList.add('task__item');
+   div.classList.add('task-info__container')
+   p.innerText = title
+   console.log(title);
+  if(type === 'Urgente'){
+     span.classList.add("span-urgent")
+   } else if(type === "Importante"){
+     span.classList.add("span-important")
+   } else if(type === "Normal"){
+     span.classList.add("span-normal")
+   }
+   button.classList.add("task__button--remove-task")
+
+   div.appendChild(span)
+   div.appendChild(p)
+   li.appendChild(div)
+   li.appendChild(button)
+   return li
+ }
+ 
+ console.log(createTaskItem());
+
+
+
+/*
+
+function createTaskItem(tasks){
+
+  const li = document.createElement('li')
+  const div = document.createElement('div')
+  const span = document.createElement('span')
+  const p = document.createElement('p')
+  const button = document.createElement('button')
+
+  li.classList.add("task__item")
+  div.classList.add("task-info__container")
+  p.innerText = tasks.title
+
+  if(tasks.type === 'Urgente'){
+     span.classList.add("span-urgent")
+  } else if(tasks.type === "Importante"){
+    span.classList.add("span-important")
+  } else if(tasks.type === "Normal"){
+    span.classList.add("span-normal")
+  }
+ button.classList.add('task__button--remove-task')
+
+ 
+  div.appendChild(span)
+  div.appendChild(p)
+  li.appendChild(div) 
+  li.appendChild(button)
+  console.log(li); 
+ }
+createTaskItem(tasks) */
+
+
+
+
+
+
+
+
+
+
+
+
+
