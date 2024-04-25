73d2fbe6c65e0217ba7c19bc9f7e6530cda28f59
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2024-04-25 01:32:05 -0300

    atividade de exclui tarefa

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const tasks = [
 
 
 
+
 function renderElements(arry){
   
   const ul = document.querySelector('ul')
@@ -24,6 +25,7 @@ function renderElements(arry){
   for(let i = 0; i < arry.length; i++){
   
     const task = arry[i]
+    
   
     const taskItem = createTaskItem(arry[i].title, arry[i].type)
     
@@ -36,21 +38,61 @@ function renderElements(arry){
 renderElements(tasks);
 
 
+function createTaskItem(title, type, index) {
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+  const p = document.createElement('p');
+  const button = document.createElement('button');
+
+  button.addEventListener('click', function() {
+    // Ao clicar no botão, remove o objeto do array tasks
+    tasks.splice(index, 1);
+    renderElements(tasks); // Renderiza novamente as tarefas atualizadas
+  });
+
+  li.classList.add('task__item');
+  div.classList.add('task-info__container');
+  p.innerText = title;
+  button.classList.add("task__button--remove-task");
+
+  if (type.toLowerCase() === 'Urgente'.toLowerCase()) {
+    span.classList.add("span-urgent");
+  } else if (type.toLowerCase() === "Importante".toLowerCase()) {
+    span.classList.add("span-important");
+  } else if (type.toLowerCase() === "Normal".toLowerCase()) {
+    span.classList.add("span-normal");
+  }
+
+  div.appendChild(span);
+  div.appendChild(p);
+  li.appendChild(div);
+  li.appendChild(button);
+
+  return li;
+}
 
 
-
+/*
 function createTaskItem(title,type){
   const li = document.createElement('li')
   const div = document.createElement('div')
   const span = document.createElement('span')
   const p = document.createElement('p')
   const button = document.createElement('button')
+ button.addEventListener('click', function(){
+  
+  renderElements(tasks)
+ 
+ })
+
 
   
    li.classList.add('task__item');
    div.classList.add('task-info__container')
    p.innerText = title
-   
+    button.classList.add("task__button--remove-task")
+  
   if(type.toLowerCase() === 'Urgente'.toLowerCase()){
      span.classList.add("span-urgent")
    } else if(type.toLowerCase() === "Importante".toLowerCase()){
@@ -58,44 +100,89 @@ function createTaskItem(title,type){
    } else if(type.toLowerCase() === "Normal".toLowerCase()){
      span.classList.add("span-normal")
    }
-   button.classList.add("task__button--remove-task")
+   
+  
 
+   
    div.appendChild(span)
    div.appendChild(p)
    li.appendChild(div)
    li.appendChild(button)
+
+
+
    return li
  }
+ */
  
+  
+ function novaTarefa(){
+  const buttonInput = document.getElementsByClassName("form__button--add-task")[0]
+ buttonInput.addEventListener('click',function(event){
+  event.preventDefault()
+
+  const inputForm = document.getElementById("input_title")
+  const selectForm = document.getElementsByClassName("form__input--priority input__box")[0]
+  const novaTarefa = {title:inputForm.value, type:selectForm.value}
+  tasks.unshift(novaTarefa)
  
-const buttonInput = document.getElementsByClassName("form__button--add-task")[0]
-const inputForm = document.getElementById("input_title")
-const selectForm = document.getElementsByClassName("form__input--priority input__box")[0]
-//console.log(inputForm);
+ renderElements(tasks)
+ })
+ }
+ novaTarefa()
 
-buttonInput.addEventListener('click',function(event){
- event.preventDefault()
- const novaTarefa = {title:inputForm.value, type:selectForm.value}
- tasks.unshift(novaTarefa)
+const buttonSection = document.getElementsByClassName("task__button--remove-task")
+const button = document.querySelectorAll(".task__button--remove-task")
+console.log(button);
 
-renderElements(tasks)
+ 
+const butaoSection = document.querySelectorAll(".task__button--remove-task")[0]
+butaoSection.addEventListener('click', function(){
+ 
+
 })
 
+/*  //console.log(tasks.indexOf( {title: "Comprar comida para o gato", type: "Urgente"}));
+  const index = tasks.findIndex(task => task.title === "Beber água");* let index2 = {title: "Comprar comida para o gato", type: "Urgente"}
+const index = tasks.findIndex(task => task.title === "Beber água");
+
+console.log(index);*/
+
+/*
+const buttaoSction = document.getElementsByClassName("task__button--remove-task")[0]
+ const buttons = document.querySelectorAll('.task__button--remove-task');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const li = button.parentNode;
+    const paragraph = li.querySelector('p');
+    const span = li.querySelector('span')
+    console.log(span);
+    const content = paragraph.textContent;
+    console.log(content);
+  });
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
-console.log(tasks);
-/*const tasks = [
-  {title: "Comprar comida para o gato", type: "Urgente"},
-  {title: "Consertar Computador", type: "Importante"},
-  {title: "Beber água", type: "Normal"},
-  {title: "Enviar relatório trimestral", type: "Importante"},
-  {title: "Fazer exercícios físicos", type: "Normal"},
-  {title: "Agendar consulta médica", type: "Urgente"},
-  {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
-  {title: "Limpar a despensa", type: "Importante"},
-  {title: "Pagar a conta de energia", type: "Urgente"},
-  {title: "Assistir a um documentário interessante", type: "Normal"},
-];*/
+
+
+
 
 
 
