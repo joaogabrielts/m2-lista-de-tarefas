d3372bbc5f8ac11d25a8c95abbc01d944ccb651c
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2024-04-25 09:24:56 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -13,56 +13,48 @@ const tasks = [
 
 
 
-
-
-
 function renderElements(arry){
-  
   const ul = document.querySelector('ul')
-
   ul.innerHTML = ''
-
   for(let i = 0; i < arry.length; i++){
-  
     const task = arry[i]
-    
-  
-    const taskItem = createTaskItem(arry[i].title, arry[i].type)
-    
-    
-   ul.appendChild(taskItem);
-    
-  }
-  
+    const taskItem = createTaskItem(task)
+   ul.appendChild(taskItem)
+
+  }  
 }
 renderElements(tasks);
 
 
-function createTaskItem(title, type, index) {
+function createTaskItem(task) {
   const li = document.createElement('li');
   const div = document.createElement('div');
   const span = document.createElement('span');
   const p = document.createElement('p');
   const button = document.createElement('button');
 
-  button.addEventListener('click', function() {
-    // Ao clicar no botão, remove o objeto do array tasks
+  button.addEventListener('click', function(index) {
+   
     tasks.splice(index, 1);
-    renderElements(tasks); // Renderiza novamente as tarefas atualizadas
+    renderElements(tasks); 
+   
   });
+  
+  if(task.type.toLowerCase() === 'Urgente'.toLowerCase()){
+    span.classList.add("span-urgent")
+  }else if(task.type.toLowerCase() === 'Normal'.toLowerCase()){
+    span.classList.add("span-normal")
+  }else if(task.type.toLowerCase() === 'Importante'.toLowerCase()){
+    span.classList.add("span-important")
+  }
+
 
   li.classList.add('task__item');
   div.classList.add('task-info__container');
-  p.innerText = title;
+  p.innerText = task.title
   button.classList.add("task__button--remove-task");
 
-  if (type.toLowerCase() === 'Urgente'.toLowerCase()) {
-    span.classList.add("span-urgent");
-  } else if (type.toLowerCase() === "Importante".toLowerCase()) {
-    span.classList.add("span-important");
-  } else if (type.toLowerCase() === "Normal".toLowerCase()) {
-    span.classList.add("span-normal");
-  }
+ 
 
   div.appendChild(span);
   div.appendChild(p);
@@ -72,50 +64,6 @@ function createTaskItem(title, type, index) {
   return li;
 }
 
-
-/*
-function createTaskItem(title,type){
-  const li = document.createElement('li')
-  const div = document.createElement('div')
-  const span = document.createElement('span')
-  const p = document.createElement('p')
-  const button = document.createElement('button')
- button.addEventListener('click', function(){
-  
-  renderElements(tasks)
- 
- })
-
-
-  
-   li.classList.add('task__item');
-   div.classList.add('task-info__container')
-   p.innerText = title
-    button.classList.add("task__button--remove-task")
-  
-  if(type.toLowerCase() === 'Urgente'.toLowerCase()){
-     span.classList.add("span-urgent")
-   } else if(type.toLowerCase() === "Importante".toLowerCase()){
-     span.classList.add("span-important")
-   } else if(type.toLowerCase() === "Normal".toLowerCase()){
-     span.classList.add("span-normal")
-   }
-   
-  
-
-   
-   div.appendChild(span)
-   div.appendChild(p)
-   li.appendChild(div)
-   li.appendChild(button)
-
-
-
-   return li
- }
- */
- 
-  
  function novaTarefa(){
   const buttonInput = document.getElementsByClassName("form__button--add-task")[0]
  buttonInput.addEventListener('click',function(event){
@@ -131,37 +79,7 @@ function createTaskItem(title,type){
  }
  novaTarefa()
 
-const buttonSection = document.getElementsByClassName("task__button--remove-task")
-const button = document.querySelectorAll(".task__button--remove-task")
-console.log(button);
 
- 
-const butaoSection = document.querySelectorAll(".task__button--remove-task")[0]
-butaoSection.addEventListener('click', function(){
- 
-
-})
-
-/*  //console.log(tasks.indexOf( {title: "Comprar comida para o gato", type: "Urgente"}));
-  const index = tasks.findIndex(task => task.title === "Beber água");* let index2 = {title: "Comprar comida para o gato", type: "Urgente"}
-const index = tasks.findIndex(task => task.title === "Beber água");
-
-console.log(index);*/
-
-/*
-const buttaoSction = document.getElementsByClassName("task__button--remove-task")[0]
- const buttons = document.querySelectorAll('.task__button--remove-task');
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const li = button.parentNode;
-    const paragraph = li.querySelector('p');
-    const span = li.querySelector('span')
-    console.log(span);
-    const content = paragraph.textContent;
-    console.log(content);
-  });
-});
-*/
 
 
 
